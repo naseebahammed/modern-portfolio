83ed482b24008d42feb2665402494078456c65ee
Naseeb Ahammed <naseebnas98@gmail.com>
2022-07-31 19:52:17 +0530

    Contact Us Responsive Complete

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.scss";
 import Contact from "./Pages/Contact/Contact";
 import Experience from "./Pages/Experience/Experience";
 import Home from "./Pages/Home/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Quote from "./Pages/Quote/Quote";
 import Skills from "./Pages/Skills/Skills";
 import Navbar from "./components/Navbar/Navbar";
@@ -10,19 +10,12 @@ import Navbar from "./components/Navbar/Navbar";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Home />
-        <Quote />
-        {/* <Experience />
-        <Skills />
-        <Contact /> */}
-      </BrowserRouter>
-
-      {/* <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/quote" element={<Quote />} />
-        </Routes> */}
+      <Navbar />
+      <Home />
+      <Quote />
+      <Experience />
+      <Skills />
+      <Contact />
     </div>
   );
 }
--- src/Pages/Skills/Skills.jsx
+++ src/Pages/Skills/Skills.jsx
@@ -6,31 +6,23 @@ const Skills = () => {
   return (
     <div className="container">
       <div className="skills">
-        <div className="wrapper">
-          <div className="skills-wrapper">
-            <div className="skills-header">
-              <div className="experience-header_heading">Skills</div>
-              <p className="experience-header_para">
-                These are the place and times, that made me, who I am
-              </p>
-            </div>
-
-            <ul className="skills-items">
-              {skillsData &&
-                skillsData.map((skills) => {
-                  return (
-                    <li key={skills.id}>
-                      <img
-                        src={skills.img}
-                        alt=""
-                        onMouseEnter={skills.imgHover}
-                      />
-                    </li>
-                  );
-                })}
-            </ul>
-          </div>
+        <div className="skills-header">
+          <div className="experience-header_heading">Skills</div>
+          <p className="experience-header_para">
+            These are the place and times, that made me, who I am
+          </p>
         </div>
+
+        <ul className="skills-items">
+          {skillsData &&
+            skillsData.map((skills) => {
+              return (
+                <li key={skills.id}>
+                  <img src={skills.img} alt="" onMouseEnter={skills.imgHover} />
+                </li>
+              );
+            })}
+        </ul>
       </div>
     </div>
   );
